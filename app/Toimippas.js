84390fbccae4296177c067db008c875ffebd036a
pekka-m <propeke@gmail.com>
2017-01-13 21:43:52 +0200

    clean

--- app/Toimippas.js
+++ app/Toimippas.js
@@ -11,24 +11,3 @@ const App = () => (
 );
 
 AppRegistry.registerComponent('Toimippas', () => App);
-
-// <NavigationProvider context={navigationContext}>
-//       <StackNavigation initialRoute={router.getRoute('ukkelit')} />
-//     </NavigationProvider>
-// import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-// import UkkelitContainer from './pages/ukkelit/UkkelitContainer';
-// import TeamContainer from './pages/team/TeamContainer';
-// import SettingsContainer from './pages/settings/SettingsContainer';
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <Router history={browserHistory}>
-//       <Route path="/" component={AppContainer}>
-//         <IndexRoute component={UkkelitContainer} />
-//         <Route path="ukkelit" component={UkkelitContainer} />
-//         <Route path="team(/:member)" component={TeamContainer} />
-//         <Route path="settings" component={SettingsContainer} />
-//       </Route>
-//     </Router>
-//   </Provider>,
-//   document.getElementById('root'),
-// );
